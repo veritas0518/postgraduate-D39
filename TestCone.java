a92cf1387ef6760c006d650812b8838031ad7143
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-15 16:03:43 +0800

    Finding the volume and bottom area of a cone
    【问题描述】定义一个圆锥类Cone，为其设置私有成员变量：radius(底面半径)和height(高)。添加两个参数的构造方法，用两个参数分别初始化两个成员变量；添加无参构造方法，无参构造方法的方法体为空。
    公有的方法getVolume()返回圆锥的体积，方法getArea()返回圆锥的底面积；一个参数的方法setRadius()和setHeight()分别用参数设定圆锥的底面半径和高；getRadius()和getHeight()分别返回圆锥的底面半径和高。
    在TestCone类的main()方法中创建Cone类的对象c1，c1的底面半径和高均从键盘输入，然后输出c1的底面积和体积。再用默认构造方法创建Cone类的对象c2，将c2的底面半径设置为c1的底面半径加1，将c2的高设置为c1的高加2，再输出c2的底面积和体积。输入输出格式如样例所示。
    【样例输入1】
    3 4
    【样例输出1】
    The floorage of c1 is 28.274333882308138, and the volume is 37.69911184307752
    The floorage of c2 is 50.26548245743669, and the volume is 100.53096491487338
    
    【样例输入2】
    5 8
    【样例输出2】
    The floorage of c1 is 78.53981633974483, and the volume is 209.43951023931956
    The floorage of c2 is 113.09733552923255, and the volume is 376.9911184307752

---
+++ TestCone.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+import java.math.*;
+public class TestCone {
+    public static void main(String[] args) {
+        Cone c1=new Cone();
+        Scanner s=new Scanner(System.in);
+        double radius=s.nextDouble();
+        double height=s.nextDouble();
+        c1.setHeight(height);
+        c1.setRadius(radius);
+        System.out.printf("The floorage of c1 is %.14f,and the volume is %.14f\n",c1.getArea(),c1.getVolume());
+
+        Cone c2 = new Cone(radius+1,height+2);
+        System.out.printf("The floorage of c2 is %.14f,and the volume is %.14f",c2.getArea(),c2.getVolume());
+    }
+}
+
+
+
+class Cone{
+    private double radius;
+    double height;
+
+
+Cone(){}
+
+Cone(double radius,double height)
+    {
+        this.radius=radius;
+        this.height=height;
+    }
+    public double getVolume(){
+        return (Math.PI*radius*radius*height)/3;
+    }
+public double getArea(){
+    return Math.PI*radius*radius;}
+
+    public void setRadius(double radius){
+this.radius=radius;
+}
+
+    public void setHeight(double height){
+this.height=height;
+
+}
+    public double getRadius(){
+return this.radius;
+}
+
+    public double getHeight(){
+    return this.height; }
+
+
+}
+
